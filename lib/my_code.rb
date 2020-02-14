46f6f809660024bfacc735684d3b685c9386540a
Jacky Lin <jacky.jia.lin@gmail.com>
2020-02-14 18:50:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(array)
+  new = []
+  counter = 0 
+  while counter < array.length do 
+     new.push(yield (array[counter])) 
+     counter += 1 
+  end 
+new 
+end 
+
+def reduce(array, starting_point = 0)
+   new = []
+   counter = 0 
+   while counter < array.length do
+     if 
+    counter += 1 
+   end 
+   end 
+ true  
+end 
